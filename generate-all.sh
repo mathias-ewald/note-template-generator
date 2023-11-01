1851342ce26ad4a37d069b85faa181e48f3f2501
Mathias Ewald <mathias.ewald@googlemail.com>
2023-11-01 09:06:27 +0100

    Add dotted paper

--- generate-all.sh
+++ generate-all.sh
@@ -1,7 +1,7 @@
 for theme in light dark; do
   for orientation in portrait landscape; do
-    for style in blank grid lines; do
-      theme=$theme orientation=$orientation style=$style bash generate.sh
+    for style in blank grid lines dotted; do
+      theme=$theme orientation=$orientation style=$style bash generate-template.sh
     done
   done
 done
--- generate-template.sh
+++ generate-template.sh
@@ -12,12 +12,13 @@ landscape_height=595
 
 theme="${theme:-light}"
 theme_light_bg_color="#FFFFFF"
-theme_light_grid_color="#EEEEEE"
+theme_light_pattern_color="#EEEEEE"
 theme_dark_bg_color="#1C1C1C"
-theme_dark_grid_color="#636363"
+theme_dark_pattern_color="#636363"
 
 style="${style:-blank}"
 pattern_spacing=15
+dot_size=1
 
 ### CONFIGURATION END ###
 
@@ -29,10 +30,10 @@ if [ $orientation == "landscape" ]; then
 fi
 
 bg_color=$theme_light_bg_color
-grid_color=$theme_light_grid_color
+pattern_color=$theme_light_pattern_color
 if [ $theme == "dark" ]; then
   bg_color=$theme_dark_bg_color
-  grid_color=$theme_dark_grid_color
+  pattern_color=$theme_dark_pattern_color
 fi
 
 name="template-$theme-$orientation-$style"
@@ -41,12 +42,23 @@ convert -size ${width}x${height} xc:$bg_color $name.$format
 
 if [ $style == "grid" ] || [ $style == "lines" ]; then
   for ((i=$pattern_spacing; i<$height; i+=$pattern_spacing)); do
-      convert $name.$format -stroke $grid_color -strokewidth 1 -draw "line 0,$i $width,$i" $name.$format
+      convert $name.$format -stroke $pattern_color -strokewidth 1 -draw "line 0,$i $width,$i" $name.$format
   done
 fi 
 
 if [ $style == "grid" ]; then
   for ((i=$pattern_spacing; i<$width; i+=$pattern_spacing)); do
-      convert $name.$format -stroke $grid_color -strokewidth 1 -draw "line $i,0 $i,$height" $name.$format
+      convert $name.$format -stroke $pattern_color -strokewidth 1 -draw "line $i,0 $i,$height" $name.$format
   done
 fi
+
+if [ $style == "dotted" ]; then
+  draw_commands=""
+  for ((i=$pattern_spacing; i<$height; i+=$pattern_spacing)); do
+      for ((j=$pattern_spacing; j<$width; j+=$pattern_spacing)); do
+          draw_commands="${draw_commands} fill $pattern_color circle $j,$i $(($j+$dot_size)),$i"
+      done
+  done
+  convert $name.$format -fill "$pattern_color" -draw "$draw_commands" $name.$format
+fi
+
